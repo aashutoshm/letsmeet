65a134b917a89500be66521865d8877df7726177
webdev0101 <hesimiao2019@gmail.com>
2021-02-01 17:18:43 +0800

    fixed dashboard

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -12,16 +12,18 @@ class MainController < ApplicationController
         # Store invite token
         session[:invite_token] = params[:invite_token] if params[:invite_token] && invite_registration
 
-        @meetings_this_week = Schedule.where("user_id = :user_id AND ((start_date >= :beginning_of_week AND start_date <= :end_of_week) OR (end_date >= :beginning_of_week AND end_date <= :end_of_week) OR (start_date <= :beginning_of_week AND end_date >= :end_of_week) OR (is_repeat = true))", {
+        @meetings_this_week = Schedule.where("user_id = :user_id AND ((start_date >= :beginning_of_week AND start_date <= :end_of_week) OR (end_date >= :beginning_of_week AND end_date <= :end_of_week) OR (start_date <= :beginning_of_week AND end_date >= :end_of_week) OR (is_repeat = true AND (repeat_type = 'd' OR repeat_type='w' OR (repeat_type='m' AND CAST (repeat_day AS INTEGER) BETWEEN :start_day AND :end_day))))", {
             :user_id => current_user.id,
             :beginning_of_week => Date.today.beginning_of_week,
             :end_of_week => Date.today.end_of_week,
-            # :week_day => Date.today.wday
+            :start_day => Date.today.beginning_of_week.day,
+            :end_day => Date.today.end_of_week.day,
         }).count
-        @meetings_this_month = Schedule.where("user_id = :user_id AND ((start_date >= :beginning_of_month AND start_date <= :end_of_month) OR (end_date >= :beginning_of_month AND end_date <= :end_of_month) OR (start_date <= :beginning_of_month AND end_date >= :end_of_month) OR (is_repeat = true))", {
+        @meetings_this_month = Schedule.where("user_id = :user_id AND ((start_date >= :beginning_of_month AND start_date <= :end_of_month) OR (end_date >= :beginning_of_month AND end_date <= :end_of_month) OR (start_date <= :beginning_of_month AND end_date >= :end_of_month) OR (is_repeat = true AND (repeat_type = 'd' OR repeat_type='w' OR repeat_type='m' OR (repeat_type='y' AND CAST (split_part(repeat_day, '/', 1) AS INTEGER) = :repeat_month))))", {
             :user_id => current_user.id,
             :beginning_of_month => Date.today.beginning_of_month,
-            :end_of_month => Date.today.end_of_month
+            :end_of_month => Date.today.end_of_month,
+            :repeat_month => Date.today.month
         }).count
         @meetings_this_year = Schedule.where("user_id = :user_id AND ((start_date >= :beginning_of_year AND start_date <= :end_of_year) OR (end_date >= :beginning_of_year AND end_date <= :end_of_year) OR (start_date <= :beginning_of_year AND end_date >= :end_of_year) OR (is_repeat = true))", {
             :user_id => current_user.id,
