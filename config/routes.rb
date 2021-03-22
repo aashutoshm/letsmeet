13949bfe385297704e53ead77026b4fd2ff6100e
webdev0101 <hesimiao2019@gmail.com>
2021-03-23 03:51:40 +0800

    added invite

--- config/routes.rb
+++ config/routes.rb
@@ -25,10 +25,10 @@ Rails.application.routes.draw do
 
     get '/health_check', to: 'health_check#all'
 
-    # Error routes.
-    match '/401', to: 'errors#unauthorized', via: :all, as: :unauthorized
-    match '/404', to: 'errors#not_found', via: :all, as: :not_found
-    match '/500', to: 'errors#internal_error', via: :all, as: :internal_error
+    # # Error routes.
+    # match '/401', to: 'errors#unauthorized', via: :all, as: :unauthorized
+    # match '/404', to: 'errors#not_found', via: :all, as: :not_found
+    # match '/500', to: 'errors#internal_error', via: :all, as: :internal_error
 
     # Signin/Signup routes.
     get '/signin', to: 'sessions#signin', as: :signin
